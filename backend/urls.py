3de4c363cd0ed0c363af1332ffde0f6ed817efa4
ren4ik <mailren82@gmail.com>
2021-06-21 10:47:15 +0500

    Изменение классов предствлений API filter

--- backend/urls.py
+++ backend/urls.py
@@ -1,11 +1,13 @@
-from django.urls import path
-from .views import CategoryView, BookView, SingleBookView
+from django.conf.urls import url
+from django.urls import path, re_path
+from .views import CategoryView, BookView, SingleBookView, SingleCategoryView, FilterAllView
 
 app_name = "backend"
-# app_name will help us do a reverse look-up latter.
+
 urlpatterns = [
     path('categories/', CategoryView.as_view()),
-    path('categories/<slug:slug>', CategoryView.as_view()),
+    path('categories/<int:pk>/', SingleCategoryView.as_view()),
     path('books/', BookView.as_view()),
-    path('books/<int:pk>/', SingleBookView.as_view())
+    path('books/<int:pk>/', SingleBookView.as_view()),
+    re_path(r'^books/filter/(?P<author>\w+)&(?P<name>\w+)&(?P<year>\d{4})/$', FilterAllView.as_view())
 ]
--- backend/views.py
+++ backend/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import get_object_or_404
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView, ListAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
+
+from django.db.models import Q
 
 from catalog.models import Category, Book
 from .serializers import CategoriesSerializer, BooksSerializer
@@ -31,3 +33,25 @@ class BookView(ListCreateAPIView):
 class SingleBookView(RetrieveUpdateDestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BooksSerializer
+
+
+class FilterAllView(ListAPIView):
+    serializer_class = BooksSerializer
+
+    def get_queryset(self):
+        author = self.kwargs['author']
+        name = self.kwargs['name']
+        year = self.kwargs['year']
+        return Book.objects.filter(
+            Q(author__icontains=author) |
+            Q(name__icontains=name) |
+            Q(date_of_pub__year=year)
+        )
+
+
+# class FilterBookView(ListAPIView):
+#     serializer_class = BooksSerializer
+#
+#     def get_queryset(self):
+#         name = self.kwargs['name']
+#         return Book.objects.filter(author__icontains=name)
